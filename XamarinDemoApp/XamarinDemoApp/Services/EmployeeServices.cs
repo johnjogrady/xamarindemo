ae93f9c0b0c1eba4f15cd51ed9ba73cdebeee4be
johnjogrady <johnjogrady@yahoo.co.uk>
2017-10-22 21:59:01 +0100

    Add project files.

---
+++ XamarinDemoApp/XamarinDemoApp/Services/EmployeeServices.cs
@@ -0,0 +1,22 @@
+﻿using Plugin.RestClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinDemoApp.Models;
+
+namespace XamarinDemoApp.Services
+{
+    public class EmployeeServices
+    {
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            RestClient<Employee> restClient = new RestClient<Employee>();
+            var employeesList = await restClient.GetAsync();
+           
+            return employeesList;
+
+        }
+    }
+}
---
+++ XamarinDemoApp/XamarinDemoApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinDemoApp.Annotations;
+using XamarinDemoApp.Models;
+using XamarinDemoApp.Services;
+
+namespace XamarinDemoApp.ViewModels
+{
+    public class MainViewModel: INotifyPropertyChanged
+    {
+        private List<Employee> _employeesList;
+
+        public List<Employee> EmployeesList
+        {
+            get { return _employeesList; }
+            set
+            {
+                _employeesList = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            InitialiseDataAsync();
+        }
+
+        private async Task InitialiseDataAsync()
+        {
+            var employeeServices = new EmployeeServices();
+
+            EmployeesList = await employeeServices.GetEmployeesAsync();
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
